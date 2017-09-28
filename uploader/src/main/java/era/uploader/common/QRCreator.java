8b79a214919e4091d5bd55cb3dd7142cb12ce1fc
Michael Rhodes <michaelleerhodes@gmail.com>
2017-09-28 17:52:17 -0500

    Changes for Josh's branch

--- uploader/src/main/java/era/uploader/common/QRCreator.java
+++ uploader/src/main/java/era/uploader/common/QRCreator.java
@@ -1,30 +1,22 @@
 package era.uploader.common;
 
-import com.google.common.collect.ImmutableMultimap;
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import era.uploader.data.model.Page;
 import era.uploader.data.model.Student;
 
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
-import java.util.Base64;
+import java.util.UUID;
 import java.util.concurrent.Callable;
 
 /**
  * A multithreaded QRCode creator. It creates a QR code out of a hash of the
  * combination of {@link Student#schoolId} and a unique sequence number.
  */
-public class QRCreator implements Callable<ImmutableMultimap<Student, BitMatrix>> {
-    public static final int QR_HEIGHT = 100;
-    public static final int QR_WIDTH = 100;
-    private static final int SALT_LEN = 8;
-    private static final int STRETCH_ITER = 100;
-    private static final int KEY_LENGTH = 256;
-    private static final String HASH_ALGO = "PBKDF2WithHmacSHA512";
+public class QRCreator implements Callable<Page> {
+    private static final int QR_HEIGHT = 100;
+    private static final int QR_WIDTH = 100;
     private final Student student;
     private final int sequenceNumber;
 
@@ -34,54 +26,30 @@ public class QRCreator implements Callable<ImmutableMultimap<Student, BitMatrix>
     }
 
     /**
-     * Hashes a QR id using PBKDF2WithHmacSHA512.
+     * Creates new QR code for a student. The QR code encodes Universally
+     * Unique ID. There should be a QR code for every page of every
+     * assignment.
      *
-     * @param schoolId       student's external school Id.
-     * @param sequenceNumber the unique id for the QR code we are generating
-     * @return the hash of the student's schoolId with the salt and sequenceNumber
+     * @return A page object mapping a QR code to a student.
+     * @throws Exception Most probably a QR writing exception. Callable makes
+     *                   the class throw a general exception
      */
-    public String hash(String schoolId, int sequenceNumber) {
-        byte[] salt = new byte[SALT_LEN];
-        SecureRandom rand = new SecureRandom();
-        rand.nextBytes(salt);
-        String hashedId = null;
-
-        try {
-            SecretKeyFactory factory = SecretKeyFactory.getInstance(HASH_ALGO);
-            PBEKeySpec spec = new PBEKeySpec(
-                    schoolId.toCharArray(),
-                    salt,
-                    STRETCH_ITER,
-                    KEY_LENGTH
-            );
-            byte[] hashedIdBytes = factory.generateSecret(spec).getEncoded();
-            Base64.Encoder encoder = Base64.getEncoder();
-
-            // store the hashed SchoolId with the salt and sequenceNumber
-            hashedId = encoder.encodeToString(hashedIdBytes)
-                    + ":"
-                    + encoder.encodeToString(salt)
-                    + ":"
-                    + sequenceNumber;
-
-        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-            // These shouldn't happen unless I supplied invalid string constants.
-            throw new RuntimeException(e);
-        }
-
-        return hashedId;
-    }
-
     @Override
-    public ImmutableMultimap<Student, BitMatrix> call() throws Exception {
-        String hashedId = hash(student.getSchoolId(), sequenceNumber);
-        QRCodeWriter writer = new QRCodeWriter();
+    public Page call() throws Exception {
+        String uuid = UUID.randomUUID().toString();
+        MultiFormatWriter writer = new MultiFormatWriter();
         BitMatrix qrCode = writer.encode(
-                hashedId,
+                uuid,
                 BarcodeFormat.QR_CODE,
                 QR_WIDTH,
                 QR_HEIGHT
         );
-        return ImmutableMultimap.of(student, qrCode);
+
+        return Page.builder()
+                .withUuid(uuid)
+                .withQRCode(qrCode)
+                .withSequenceNumber(sequenceNumber)
+                .byStudent(student)
+                .create();
     }
 }
--- uploader/src/main/java/era/uploader/controller/QRCreationController.java
+++ uploader/src/main/java/era/uploader/controller/QRCreationController.java
@@ -1,71 +1,55 @@
 package era.uploader.controller;
 
-import com.google.common.collect.ImmutableMultimap;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Multimap;
-import com.google.zxing.common.BitMatrix;
-import era.uploader.common.QRCreationException;
-import era.uploader.common.QRCreator;
-import era.uploader.data.model.Course;
-import era.uploader.data.model.Student;
-
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-
 public class QRCreationController {
     private static final String AWAIT_ERROR = "It has taken the QR creation process a really long time so we stopped it";
 
-    /**
-     * Provided a course with students and a number of QR codes to generate
-     * for each student this method generates QR codes for students. The QR
-     * codes will be a hash of the students schoolId with a sequence number
-     * for the page.
-     *
-     * @param course      A course filled with students.
-     * @param numberOfQRs the number of qr codes we should generate for each
-     *                    student
-     */
-    public Multimap<Student, BitMatrix> createQRs(Course course, int numberOfQRs)
-            throws QRCreationException {
-        int processors = Runtime.getRuntime().availableProcessors();
-        ExecutorService threads = Executors.newFixedThreadPool(processors);
-        List<Future<ImmutableMultimap<Student, BitMatrix>>> futures =
-                Lists.newArrayList();
-
-        for (int i = 0; i < numberOfQRs; i++) {
-            for (Student student : course.getStudents()) {
-                QRCreator creator = new QRCreator(student, i);
-                futures.add(threads.submit(creator));
-            }
-        }
-
-        threads.shutdown();
-
-        try {
-            if (!threads.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS)) {
-                throw new QRCreationException(AWAIT_ERROR);
-            }
-        } catch (InterruptedException e) {
-            throw new QRCreationException(e);
-        }
-
-        ImmutableMultimap.Builder<Student, BitMatrix> studentsToQRs =
-                ImmutableMultimap.builder();
-
-        for (Future<ImmutableMultimap<Student, BitMatrix>> singletonFuture : futures) {
-            try {
-                studentsToQRs.putAll(singletonFuture.get());
-            } catch (InterruptedException e) {
-                throw new QRCreationException(e);
-            } catch (ExecutionException e) {
-                throw new QRCreationException(e.getMessage());
-            }
-        }
-
-        return studentsToQRs.build();
-    }
+//    /**
+//     * Provided a course with students and a number of QR codes to generate
+//     * for each student this method generates QR codes for students. The QR
+//     * codes will be a hash of the students schoolId with a sequence number
+//     * for the page.
+//     *
+//     * @param course      A course filled with students.
+//     * @param numberOfQRs the number of qr codes we should generate for each
+//     *                    student
+//     */
+//    public Multimap<Student, BitMatrix> createQRs(Course course, int numberOfQRs)
+//            throws QRCreationException {
+//        int processors = Runtime.getRuntime().availableProcessors();
+//        ExecutorService threads = Executors.newFixedThreadPool(processors);
+//        List<Future<ImmutableMultimap<Student, BitMatrix>>> futures =
+//                Lists.newArrayList();
+//
+//        for (int i = 0; i < numberOfQRs; i++) {
+//            for (Student student : course.getStudents()) {
+//                QRCreator creator = new QRCreator(student, i);
+//                futures.add(threads.submit(creator));
+//            }
+//        }
+//
+//        threads.shutdown();
+//
+//        try {
+//            if (!threads.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS)) {
+//                throw new QRCreationException(AWAIT_ERROR);
+//            }
+//        } catch (InterruptedException e) {
+//            throw new QRCreationException(e);
+//        }
+//
+//        ImmutableMultimap.Builder<Student, BitMatrix> studentsToQRs =
+//                ImmutableMultimap.builder();
+//
+//        for (Future<ImmutableMultimap<Student, BitMatrix>> singletonFuture : futures) {
+//            try {
+//                studentsToQRs.putAll(singletonFuture.get());
+//            } catch (InterruptedException e) {
+//                throw new QRCreationException(e);
+//            } catch (ExecutionException e) {
+//                throw new QRCreationException(e.getMessage());
+//            }
+//        }
+//
+//        return studentsToQRs.build();
+//    }
 }
