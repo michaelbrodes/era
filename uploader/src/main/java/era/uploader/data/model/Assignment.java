2bd475afab54f67dffdf576c382cfa0c37569a56
Cam Scott <camscot@Cams-MacBook-Pro.local>
2017-10-11 16:35:22 -0500

    Made changes to the Assignment class, as well as minor changes to Student class. Still need to finish method in PDFProcessor.java

--- uploader/src/main/java/era/uploader/data/model/Assignment.java
+++ uploader/src/main/java/era/uploader/data/model/Assignment.java
@@ -1,4 +1,47 @@
 package era.uploader.data.model;
 
+import java.util.Set;
+import java.util.HashSet;
+
 public class Assignment {
+    /* Class Fields */
+    private String imageFilePath;               /* Path to the PDF file with the images associated with the assignment */
+    private String name;                        /* Name of the Assignment */
+    private Set<Page> pages = new HashSet<>();  /* Set of Page objects for each Assignment */
+
+    /* Constructor */
+    public Assignment(
+            String imageFilePath,
+            String name,
+            Set<Page> pages
+    ) {
+        this.imageFilePath = imageFilePath;
+        this.name = name;
+        this.pages = pages;
+    }
+
+    /* Getters and Setters */
+    public String getImageFilePath() {
+        return imageFilePath;
+    }
+
+    public void setImageFilePath(String imageFilePath) {
+        this.imageFilePath = imageFilePath;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Page> getPages() {
+        return pages;
+    }
+
+    public void setPages(Set<Page> pages) {
+        this.pages = pages;
+    }
 }
--- uploader/src/main/java/era/uploader/processing/PDFProcessor.java
+++ uploader/src/main/java/era/uploader/processing/PDFProcessor.java
@@ -1,4 +1,13 @@
 package era.uploader.processing;
 
+import era.uploader.data.model.Assignment;
+
 public class PDFProcessor {
+
+    /* Given a singular UUID, find and update a Placeholder page into a
+     * Valid page
+     */
+    void associateStudentsWithPage(Assignment assignment, long uuid) {
+
+    }
 }
