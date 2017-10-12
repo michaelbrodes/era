aa450f4e33d739cd42afd2e084cc484e6e00b5be
Joshua Raben <jraben@siue.edu>
2017-10-11 19:15:30 -0500

    Created ConvertPDFtoBufferedImg method for reading QRCode on scanned pdf

--- uploader/src/main/java/era/uploader/data/model/Page.java
+++ uploader/src/main/java/era/uploader/data/model/Page.java
@@ -2,6 +2,7 @@ package era.uploader.data.model;
 
 import com.google.common.base.Preconditions;
 import com.google.zxing.common.BitMatrix;
+import org.apache.pdfbox.pdmodel.PDDocument;
 
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -18,6 +19,7 @@ public class Page {
     // calls and it should not be stored in the database
     private transient BitMatrix qrCode;
     private String uuid;
+    private transient PDDocument document;
     private Assignment assignment;
 
     private Page(Builder builder) {
@@ -26,6 +28,7 @@ public class Page {
         this.uuid = builder.uuid;
         this.qrCode = builder.qrCode;
         this.assignment = builder.assignment;
+        this.document = builder.document;
     }
 
     public static Builder builder() {
@@ -101,6 +104,13 @@ public class Page {
         private BitMatrix qrCode;
         private Assignment assignment;
         private String uuid;
+        private PDDocument document;
+
+        public Builder withDocument(PDDocument document){
+            Preconditions.checkNotNull(document);
+            this.document = document;
+            return this;
+        }
 
         public Builder byStudent(Student student) {
             Preconditions.checkNotNull(student);
--- uploader/src/main/java/era/uploader/processing/QRScanner.java
+++ uploader/src/main/java/era/uploader/processing/QRScanner.java
@@ -1,4 +1,38 @@
 package era.uploader.processing;
 
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import era.uploader.data.model.Page;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 public class QRScanner {
+    public static Page convertPDFtoBufferedImg(PDDocument document) throws IOException, NotFoundException {
+        PDFRenderer renderer = new PDFRenderer(document);
+        String tmpFinalResult = "";
+        try{
+            BufferedImage pdf = renderer.renderImageWithDPI(0, 300);
+
+            LuminanceSource tmpSource = new BufferedImageLuminanceSource(pdf);
+            BinaryBitmap tmpBitmap = new BinaryBitmap(new HybridBinarizer(tmpSource));
+            MultiFormatReader tmpQRcodeReader = new MultiFormatReader();
+            Result tmpResult;
+
+            tmpResult = tmpQRcodeReader.decode(tmpBitmap);
+            tmpFinalResult = String.valueOf(tmpResult.getText());
+            System.out.println(tmpFinalResult);
+
+        }
+        catch (IOException | NotFoundException e) {
+            System.out.println("Error: " + e );
+        }
+        return Page.builder()
+                .withDocument(document)
+                .withUuid(tmpFinalResult)
+                .create();
+    }//convertPDFtoBufferedImage
 }
