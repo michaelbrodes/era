7dc2125da860b0a9f793e3c0507310e6705db286
Zachary Auer <auerzachary@yahoo.com>
2017-11-04 20:23:42 -0500

    Minor tweaks to a couple files as I am creating the Desktop Client

--- uploader/src/main/java/era/uploader/controller/PDFScanningController.java
+++ uploader/src/main/java/era/uploader/controller/PDFScanningController.java
@@ -1,12 +1,8 @@
 package era.uploader.controller;
 
-<<<<<<< HEAD
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
-=======
 import era.uploader.data.PageDAO;
 import era.uploader.data.database.PageDAOImpl;
->>>>>>> 72f8f1bc7567fc474d707f3fa96e336d9cfccbe0
 import era.uploader.data.model.Assignment;
 import era.uploader.data.model.Course;
 import era.uploader.processing.PDFProcessor;
@@ -27,12 +23,8 @@ import java.util.Set;
 public class PDFScanningController {
     private final PageDAO pageDAO = new PageDAOImpl();
     public void scanPDF(Path pdf, Course course, String assignment) throws IOException {
-<<<<<<< HEAD
-        //System.out.print("Recieved PDFs");
-        List<Assignment> assignments = PDFProcessor.process(pdf, course, assignment);
-=======
+
         List<Assignment> assignments = PDFProcessor.process(pageDAO, pdf, course, assignment);
->>>>>>> 72f8f1bc7567fc474d707f3fa96e336d9cfccbe0
 
     }
 
---
+++ uploader/src/main/java/era/uploader/processing/AssignmentUploader.java
@@ -0,0 +1,19 @@
+package era.uploader.processing;
+
+import era.uploader.data.model.Assignment;
+
+import java.util.Collection;
+import org.apache.http.HttpResponse;
+//import org.apache.http.client.fluent.Form;
+//import org.apache.http.client.fluent.Request;
+import org.apache.http.entity.ContentType;
+
+public class AssignmentUploader {
+
+    public void uploadAssignments(Collection<Assignment> assignments) {
+
+
+
+    }
+
+}
